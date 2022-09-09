e0bcc5c25c7e91b0ff529a1b406d9d210c0b0c4f
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-09 14:22:48 +0200

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är ditt favorit Bilmärke?");
+            string Bil = Console.ReadLine();
+            Console.WriteLine("märken hetter Mazerati");
+            string Märken = Console.ReadLine();
+
+            Console.WriteLine("Hej" + "Jag hoppas du år ditt drömmbil" + Märken + Bil + ".");
+        }
+    }
+}
